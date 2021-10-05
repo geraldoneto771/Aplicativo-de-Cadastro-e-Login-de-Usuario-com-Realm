ae528218e73caebcb3d9f992e5edf9427d534cb4
geraldo.pires <geraldopireslacerda771@gmail.com>
2021-10-05 09:54:56 -0300

    XML da tela de introdução finalizado

---
+++ app/src/main/java/com/example/aplicativocadastrologinrealm/App.kt
@@ -0,0 +1,24 @@
+package com.example.aplicativocadastrologinrealm
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+
+        val conf = RealmConfiguration.Builder()
+            .name("app.realm")
+            .schemaVersion(0L)
+            .allowQueriesOnUiThread(true)
+            .allowWritesOnUiThread(true)
+            .build()
+
+        Realm.setDefaultConfiguration(conf)
+
+    }
+}
---
+++ app/src/main/java/com/example/aplicativocadastrologinrealm/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.aplicativocadastrologinrealm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //vinculando o navigation com a main_activity
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment_container_view)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
